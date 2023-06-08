6d76a9bf2248bbf586512fea7ce3bf7dcf86379a
AgentBraun <AgentBraun@users.noreply.github.com>
2023-06-09 00:49:58 +0200

    save before the first prisma push

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import 'dotenv/config';
+import env from './utils/validateEnv';
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import createHttpError from 'http-errors';
+
+//routes import
+import adminRoutes from './routes/admin';
+import professorsRoutes from './routes/professor';
+import studentsRoutes from './routes/students';
+
+const port = env.PORT;
 
 const app = express();
 const prisma = new PrismaClient();
@@ -8,6 +16,14 @@ const prisma = new PrismaClient();
 app.use(express.json);
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('Data base online on port 3000');
+app.use('/api/admin', adminRoutes);
+app.use('/api/professors', professorsRoutes);
+app.use('/api/students', studentsRoutes);
+
+app.use((req, res, next) => {
+  next(createHttpError(404, 'endpoint not found!'));
+});
+
+app.listen(port, () => {
+  console.log('Data base online on port 5000');
 });
